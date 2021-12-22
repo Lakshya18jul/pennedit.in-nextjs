4e47fad8f85ff2342b138f00391168dff0d4e7be
Lakshya Agrawal <lakshya18jul.vns@gmail.com>
2021-12-22 16:56:20 +0530

    Version try 6

--- pages/question/[questionId].js
+++ pages/question/[questionId].js
@@ -35,7 +35,7 @@ export default SingleQuestionPageFunction;
 export async function getServerSideProps(context) {
   try {
     const params = context.params;
-    // const questionRef = doc(db, "questions", params.questionId);
+    const questionRef = doc(db, "questions", params.questionId);
     // const questionSnap = await getDoc(questionRef);
     // let questionSnap;
     // const unsub = onSnapshot(doc(db, "questions", params.questionId), (doc) => {
