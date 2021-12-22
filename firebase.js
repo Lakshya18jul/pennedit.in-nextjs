650de4abd40e286702581056840eb6a9b6a08dee
Lakshya Agrawal <lakshya18jul.vns@gmail.com>
2021-12-22 18:23:50 +0530

    Version Try 8

--- firebase.js
+++ firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-const db = getFirestore(app);
+const db = getFirestore();
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const storage = getStorage(app);
