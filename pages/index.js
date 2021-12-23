08dac34b9df1242336c0d70389e6e106c302394a
Lakshya Agrawal <lakshya18jul.vns@gmail.com>
2021-12-23 13:47:13 +0530

    version try 12

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Login from "../components/Login";
 import Home from "../components/Home";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import React from "react";
 
 function HomePage() {
   const [currUserState, setCurrUserState] = useRecoilState(userState);
@@ -28,7 +29,92 @@ function HomePage() {
     });
   }, []);
 
-  return <>{returnLogin ? <Login /> : <Home />}</>;
+  return (
+    <>
+      {returnLogin ? (
+        <React.Fragment>
+          <Head>
+            <meta
+              name="description"
+              content="Pennedit is a question-and-answer website which allows users to post, ask questions and answer others based on their favourite categories and keep themselves updated regarding the latest developments in their field of interest. "
+            />
+            <title>Pennedit</title>
+            <link
+              rel="stylesheet"
+              href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+              integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+              crossOrigin="anonymous"
+            />
+            <link
+              href="https://fonts.googleapis.com/css?family=Nunito"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css?family=Pacifico"
+              rel="stylesheet"
+            />
+
+            <link
+              rel="stylesheet"
+              href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+              integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+              crossOrigin="anonymous"
+            />
+            <link
+              rel="icon"
+              href="https://firebasestorage.googleapis.com/v0/b/pennedit-d53c8.appspot.com/o/pennedit-inapp-images%2Fdownload.png?alt=media&token=30917a0f-c98c-4a77-971b-3dcb02efcb54"
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1 , minimum-scale=1"
+            />
+          </Head>
+          <Login />{" "}
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          {" "}
+          <Head>
+            <meta
+              name="description"
+              content="Pennedit is a question-and-answer website which allows users to post, ask questions and answer others based on their favourite categories and keep themselves updated regarding the latest developments in their field of interest. "
+            />
+            <title>Pennedit</title>
+            <link
+              rel="stylesheet"
+              href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+              integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+              crossOrigin="anonymous"
+            />
+            <link
+              href="https://fonts.googleapis.com/css?family=Nunito"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css?family=Pacifico"
+              rel="stylesheet"
+            />
+
+            <link
+              rel="stylesheet"
+              href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+              integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+              crossOrigin="anonymous"
+            />
+            <link
+              rel="icon"
+              href="https://firebasestorage.googleapis.com/v0/b/pennedit-d53c8.appspot.com/o/pennedit-inapp-images%2Fdownload.png?alt=media&token=30917a0f-c98c-4a77-971b-3dcb02efcb54"
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1 , minimum-scale=1"
+            />
+          </Head>
+          <Home />
+        </React.Fragment>
+      )}
+    </>
+  );
 }
 
 export default HomePage;
